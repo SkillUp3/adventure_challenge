a04aaac5c7f34f5b866dfa7f4be6537ebd0ecc7c
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-04-15 09:27:00 +0900

    Create static initialization block for locations and exits

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static Map<Integer, Location> locations = new HashMap<>();
+    //    private static Map<Integer, Location> locations = new HashMap<>();
+    private static Locations locations = new Locations();
 
     public static void main(String[] args) {
         // write your code here
@@ -25,39 +26,39 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        Map<String, Integer> tempExit = new HashMap<>();
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
-
-
-        tempExit = new HashMap<>();
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 4);
-        tempExit.put("N", 5);
-//        tempExit.put("Q",0);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
-        tempExit = new HashMap<>();
-        tempExit.put("N", 5);
-//        tempExit.put("Q",0);
-        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("W", 1);
-        tempExit.put("Q", 0);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-//        tempExit.put("Q",0);
-        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
-
-        tempExit = new HashMap<>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-//        tempExit.put("Q",0);
-        locations.put(5, new Location(5, "You are in the forest",tempExit));
-
+//        Map<String, Integer> tempExit = new HashMap<>();
+//        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
+//
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("W", 2);
+//        tempExit.put("E", 3);
+//        tempExit.put("S", 4);
+//        tempExit.put("N", 5);
+////        tempExit.put("Q",0);
+//        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
+//        tempExit = new HashMap<>();
+//        tempExit.put("N", 5);
+////        tempExit.put("Q",0);
+//        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("W", 1);
+//        tempExit.put("Q", 0);
+//        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("N", 1);
+//        tempExit.put("W", 2);
+////        tempExit.put("Q",0);
+//        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
+//
+//        tempExit = new HashMap<>();
+//        tempExit.put("S", 1);
+//        tempExit.put("W", 2);
+////        tempExit.put("Q",0);
+//        locations.put(5, new Location(5, "You are in the forest", tempExit));
+//
         Map<String, String> vocabulary = new HashMap<>();
         vocabulary.put("QUIT", "Q");
         vocabulary.put("NORTH", "N");
@@ -69,7 +70,6 @@ public class Main {
         while (true) {
             System.out.println(locations.get(loc).getDescription());
 
-            tempExit.remove("S");
             if (loc == 0) {
                 break;
             }
