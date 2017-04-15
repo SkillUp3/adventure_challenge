801dbeb76686f048df82999d0eafb0d14545ea70
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-04-15 09:44:29 +0900

    FileWriter Class and Finally Block

--- src/com/company/Locations.java
+++ src/com/company/Locations.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,6 +14,28 @@ public class Locations implements Map<Integer, Location> {
 
     private static Map<Integer, Location> locations = new HashMap<>();
 
+    public static void main(String[] args) {
+        FileWriter locFile = null;
+        try {
+            locFile = new FileWriter("locations.txt");
+            for(Location location : locations.values()){
+                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("in catch block");
+            e.printStackTrace();
+        } finally{
+            System.out.println("in finally block");
+            try {
+                if(locFile != null){
+                    locFile.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
     static {
         Map<String, Integer> tempExit = new HashMap<String, Integer>();
         locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
